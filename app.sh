3086e7f239957367e14409395c8e6054e7ed2d55
aarshsqaureops <109953812+aarshsqaureops@users.noreply.github.com>
2022-10-17 12:46:54 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,7 +4,7 @@ sudo apt autoremove --purge
 sudo apt autoclean
 sudo apt install nginx unzip php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip php-fpm php-mysql -y
 sudo wget https://wordpress.org/latest.zip
-sudo unzip master.zip
+sudo unzip latest.zip
 cd wordpress/
 sudo cp -r * /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
