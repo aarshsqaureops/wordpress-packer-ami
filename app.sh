6ae2a80c82bbbb05e12ab8b0ea1c7587768e57a4
Aarsh Parashar <aarshparashar@gmail.com>
2023-07-07 00:44:35 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt autoremove --purge
-sudo apt autoclean
-sudo apt install nginx unzip php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip php-fpm php-mysql -y
+sudo apt install nginx unzip mysql-server php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip php-fpm php-mysql -y
 sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
-cd wordpress/
-sudo cp -r * /var/www/html/
+sudo cp -r wordpress/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
-cd /etc/nginx/sites-enabled/
 sudo wget https://github.com/aarshsqaureops/nginx-configuration/archive/refs/heads/main.zip
 sudo unzip main.zip
-sudo mv nginx-configuration-main/wordpress /etc/nginx/sites-enabled/
-sudo systemctl reload php7.4-fpm.service
-sudo rm -rf default main.zip nginx-configuration-main/
-sudo systemctl reload nginx
-cd ~
+sudo cp -r nginx-configuration-main/* /etc/nginx/sites-enabled/
+sudo rm -rf latest.zip wordpress/ main.zip nginx-configuration-main/ /etc/nginx/sites-enabled/default
+sudo systemctl reload php8.1-fpm.service nginx.service
 sudo wget https://s3.us-east-2.amazonaws.com/amazoncloudwatch-agent-us-east-2/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 #sudo aws configure --profile AmazonCloudWatchAgent
